7331ed5dc09c1979eff59db16fa77d3f663e64e5
anweei <47211590+anweei@users.noreply.github.com>
2019-02-26 15:29:47 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "gpio.h"
-#include "uart.h"
+#include "gpiote.h"
+#include "ppi.h"
+
 
 int main() {
-	uart_init();
 	
 	// Configure LED Matrix
 	for(int i = 4; i <= 15; i++){
@@ -15,42 +16,26 @@ int main() {
 	GPIO->PIN_CNF[17] = 0;
 	GPIO->PIN_CNF[26] = 0;
 
-	iprintf("Norway has %d counties. \n\r", 18);
+	//Configure A-button event
+	GPIOTE->CONFIG[0] = (2 << 16) | (17 << 8) | 1; //Polarity = HiToLo | PSEL = 17 | Mode = Event 
 
-	int sleep = 0;
-	while(1){
-		/* Check if button B is pressed;
-		 * send B if it is */
-		if(!(GPIO->IN & (1 << 26)) ){
-			uart_send('B');
-		}
+	//configure led-pin tasks
+	GPIOTE->CONFIG[1] = (0 << 20) | (3 << 16)| (13 << 8) | 3; //OUTINIT = 0 | Polarity = toggle | PSEL = 13 | Mode = Task 
+	GPIOTE->CONFIG[2] = (0 << 20) | (3 << 16)| (14 << 8) | 3; //OUTINIT = 0 | Polarity = toggle | PSEL = 14 | Mode = Task 
+	GPIOTE->CONFIG[3] = (0 << 20) | (3 << 16)| (15 << 8) | 3; //OUTINIT = 0 | Polarity = toggle | PSEL = 15 | Mode = Task 
 
-		/* Check if button a is pressed;
-		 * send A if it is */
-		if(!(GPIO->IN & (1 << 17)) ){
-			uart_send('A');
-		}
-		if(uart_read()){
-			if ((GPIO->OUT)&(1<<13)) {
-				for(int i=13; i<=15; i++)
-					GPIO->OUTCLR=(1<<i);
-			}else{
-				for(int i=13; i<=15; i++)
-					GPIO->OUTSET=(1<<i);
-			}
-		}
+	PPI->PPI_CH[0].EEP = (uint32_t)&(GPIOTE->IN[0]);
+	PPI->PPI_CH[0].TEP = (uint32_t)&(GPIOTE->OUT[1]);
 
-		sleep = 10000;
-		while(--sleep);
-	}
+	PPI->PPI_CH[1].EEP = (uint32_t)&(GPIOTE->IN[0]);
+	PPI->PPI_CH[1].TEP = (uint32_t)&(GPIOTE->OUT[2]);
+
+	PPI->PPI_CH[2].EEP = (uint32_t)&(GPIOTE->IN[0]);
+	PPI->PPI_CH[2].TEP = (uint32_t)&(GPIOTE->OUT[3]);
+
+	PPI->CHENSET = (1 << 0) | (1 << 1) | (1 << 2);
+
+
+
 	return 0;
-}	
-
-ssize_t _write(int fd, const void *buf, size_t count){
-	char * letter = (char *)(buf);
-	for(int i = 0; i < count; i++){
-		uart_send(*letter);
-		letter ++;
-	}
-	return count;
 }
